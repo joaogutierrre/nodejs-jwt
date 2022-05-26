f3fb0eb3e13d5c34393c26ce091165dfc6f2daf6
João Gutierre <gutierrejoao@gmail.com>
2022-05-26 18:43:03 -0300

    feat: Creating first testing route

--- index.js
+++ index.js
@@ -2,3 +2,13 @@ const express = require("express");
 const jwt = require("jsonwebtoken");
 
 const app = express();
+
+app.get("/api", (req, res) => {
+  res.json({
+    message: "Server is working",
+  });
+});
+
+app.listen(3000, (req, res) => {
+  console.log("Server runnin on port 3000");
+});
