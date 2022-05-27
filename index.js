064e4b6efd2d7d5042c53f46d4baa4058dbb9e1d
João Gutierre <gutierrejoao@gmail.com>
2022-05-27 13:22:12 -0300

    feat: Implementing token validation logic function

--- index.js
+++ index.js
@@ -15,13 +15,26 @@ app.post("/api/login", (req, res) => {
     name: "John",
     email: "john@email.com",
   };
-
-  jwt.sign({ usr: usr }, "secretkey", (err, token) => {
+ 
+  jwt.sign({
+    usr: usr
+  }, "secretkey", (err, token) => {
     res.json({
       token,
     });
   });
 });
+
+function verifyToken(req, res, next) {
+  const bearerHeader = req.headers["authorization"];
+  if (typeof bearerHeader !== "undefined") {
+    const bearerToken = bearerHeader.split(" ")[1];
+    req.token = bearerToken;
+    next();
+  } else {
+    res.sendStatus(403)
+  }
+}
 
 app.listen(3000, (req, res) => {
   console.log("Server runnin on port 3000");
